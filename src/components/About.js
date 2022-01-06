301bcc063858dbd67637146deb0993253413604f
steve <steve.huckle@gmail.com>
2022-01-06 21:25:09 +0000

    Link

--- src/components/About.js
+++ src/components/About.js
@@ -25,10 +25,10 @@ export const About = () => {
         <>
             <p><span id="title">{UIText.appTitle}</span> {UIText.appTextAbout}</p>
 
-            <p>To use <span id="title">{UIText.appTitle}</span>, you will need a <a href="https://www.flickr.com/services/api/misc.api_keys.html" target="_blank" rel="noreferrer">Flickr API Key</a>, which will you'll use to get images to classify. You will also need an <a href="https://beta.openai.com/signup" target="_blank" rel="noreferrer">Open AI Key</a> - <a href="https://openai.com/" target="_blank" rel="noreferrer" >Open AI</a> does the text generation.</p>
+            <p>To use <span id="title">{UIText.appTitle}</span>, you will need a <a href="https://www.flickr.com/services/api/misc.api_keys.html" target="_blank" rel="noreferrer">Flickr API Key</a>, which you'll use to get images to classify. You will also need an <a href="https://beta.openai.com/signup" target="_blank" rel="noreferrer">Open AI Key</a> - <a href="https://openai.com/" target="_blank" rel="noreferrer" >Open AI</a> does the text generation.</p>
 
             <p>The <a href="https://github.com/glowkeeper/storymaker" target="_blank" rel="noreferrer">source code</a> for <span id="title">{UIText.appTitle}</span> is available under a Creative Commons <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noreferrer">Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND)</a> license.</p>
-            <p><img src="https://licensebuttons.net/l/by-nc-nd/3.0/88x31.png" alt="CC BY-NC-ND"/></p>
+            <p><a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noreferrer"><img src="https://licensebuttons.net/l/by-nc-nd/3.0/88x31.png" alt="CC BY-NC-ND"/></a></p>
         </>
     )
 }
