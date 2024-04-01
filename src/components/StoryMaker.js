18600efcff0231d5757ce04b827b11155ebccefa
glowkeeper <steve.huckle@gmail.com>
2024-04-01 10:37:54 +0100

    remove unnecessary link

--- src/components/StoryMaker.js
+++ src/components/StoryMaker.js
@@ -1,5 +1,4 @@
 import { useEffect, useContext, useState } from 'react'
-import { Link } from "react-router-dom"
 import { useNavigate } from 'react-router-dom'
 
 import { StoreContext, StoreActions } from '../store/store'
